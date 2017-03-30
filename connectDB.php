206f36215c12a7ae892fb447d116eff3ba81d984
jhsu26 <hsujessica21@gmail.com>
2017-03-29 22:26:02 -0400

    PUT CORRECT DB INFO BACK

--- connectDB.php
+++ connectDB.php
@@ -1,6 +1,5 @@
 <?php
-#$conn = mysqli_connect("mydbinstance.cvjenxnjjyrk.us-west-2.rds.amazonaws.com:3306","admin", "admin123");
-#$conn = mysqli_connect("127.0.0.1:3306","admin", "rabbit");
+$conn = mysqli_connect("mydbinstance.cvjenxnjjyrk.us-west-2.rds.amazonaws.com:3306","admin", "admin123");
 
 
 if (mysqli_connect_errno()) {
@@ -13,9 +12,5 @@ if ( ! mysqli_select_db($conn, "mydbinstance") ) {
 	exit(1);
 }
 
-#if ( ! mysqli_select_db($conn, "cs370") ) {
-#	echo("Error: %s\n"+ mysqli_error($conn));
-#	exit(1);
-#
 
 ?>
