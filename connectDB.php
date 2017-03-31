3c1d8360beaf736252037e4a04e91f1f8c417194
jhsu26 <hsujessica21@gmail.com>
2017-03-31 13:02:27 -0400

    fsadfas

--- connectDB.php
+++ connectDB.php
@@ -1,5 +1,6 @@
 <?php
-$conn = mysqli_connect("mydbinstance.cvjenxnjjyrk.us-west-2.rds.amazonaws.com:3306","admin", "admin123");
+#$conn = mysqli_connect("mydbinstance.cvjenxnjjyrk.us-west-2.rds.amazonaws.com:3306","admin", "admin123");
+$conn = mysqli_connect("127.0.0.1:3306","admin", "rabbit");
 
 
 if (mysqli_connect_errno()) {
@@ -7,10 +8,14 @@ if (mysqli_connect_errno()) {
 	exit(1);
 }
 
-if ( ! mysqli_select_db($conn, "mydbinstance") ) {
+/*if ( ! mysqli_select_db($conn, "mydbinstance") ) {
+	echo("Error: %s\n"+ mysqli_error($conn));
+	exit(1);
+}*/
+
+if ( ! mysqli_select_db($conn, "cs370") ) {
 	echo("Error: %s\n"+ mysqli_error($conn));
 	exit(1);
 }
-
 
 ?>
