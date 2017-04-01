5435820e7a30f94da98bca778e77383acf786f98
jhsu26 <hsujessica21@gmail.com>
2017-04-01 15:10:51 -0400

    try again

--- welcome.php
+++ welcome.php
@@ -130,31 +130,32 @@ $( document ).ready(function() {
         });
     });
 
-	function logout() {
-		hello.logout();
-		console.log("logging out ... ");
-    	f="logout";
-    	profileType = document.getElementById('profileType').innerHTML;
-    	$.ajax({
-            url: 'ajax.php',
-            data: {func: f},
-           	type: 'post',
-            success: function(result) {
-                console.log("action performed successfully");
-                if (profileType == "dev") {
-                	window.location.href = "loginDev.php";
-                } else {
-                   	window.location.href = "loginComp.php";
-                }
-            }, 
-            error: function(result) {
-            	console.log(result);
-            }
-        });
-    	
-	}
-    
+	
 });
+function logout() {
+	hello.logout();
+	console.log("logging out ... ");
+	f="logout";
+	profileType = document.getElementById('profileType').innerHTML;
+	$.ajax({
+        url: 'ajax.php',
+        data: {func: f},
+       	type: 'post',
+        success: function(result) {
+            console.log("action performed successfully");
+            if (profileType == "dev") {
+            	window.location.href = "loginDev.php";
+            } else {
+               	window.location.href = "loginComp.php";
+            }
+        }, 
+        error: function(result) {
+        	console.log(result);
+        }
+    });
+	
+}
+
 </script>
 
 </body>
